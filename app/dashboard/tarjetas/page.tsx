9c5a15c573d30c71d4dc3a25e0d094bd5c0a0aa0
dslo_07 <sdeveloper.lm@gmail.com>
2025-11-18 20:05:45 -0300

    integracion de ui y chart

---
+++ app/dashboard/tarjetas/page.tsx
@@ -0,0 +1,15 @@
+interface MesProps{
+    params:{
+        mes: string
+    }
+}
+
+export const Ahorro = (props : MesProps) =>{
+    const { mes } = props.params
+    console.log(mes)
+    return(
+        <h1>Administracion de ahorros</h1>
+    )
+}
+
+export default Ahorro
